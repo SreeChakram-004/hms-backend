077f3376ee6dbbe07f0dcfa4f7582c83b72e81aa
Sree Chakram <srebhargav.g@avyata.com>
2023-06-30 16:54:54 +0530

    changes reverted

--- app.js
+++ app.js
@@ -50,8 +50,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Google OAuth login route
-// app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-app.get('https://wild-red-clownfish-tam.cyclic.app/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 // router.get("/auth/google",
 //   // (req, res, next) => {
 //   //   req.session.google_oauth2_state = Math.random().toString(36).substring(2);
@@ -65,25 +64,7 @@ app.get('https://wild-red-clownfish-tam.cyclic.app/auth/google', passport.authen
 // );
 
 // Google OAuth callback route
-// app.get('/auth/google/callback', passport.authenticate('google',{ session: false }), (req, res) => {
-//   // Generate JWT token
-//   const accessToken = jwt.sign({ email: req.user.email, password: 'password' }, jwtSecret, { expiresIn: '7d' });
-//   const refreshToken = jwt.sign({ email: req.user.email, password: 'password' }, jwtSecret, { expiresIn: '7d' });
-
-//   // Return the tokens as a JSON response
-//   res.json({
-//     status: true,
-//     data: {
-//       token_type: 'Bearer',
-//       expires_in: '7d',
-//       access_token: accessToken,
-//       refresh_token: refreshToken
-//     }
-//   });
-
-// });
-
-app.get('https://wild-red-clownfish-tam.cyclic.app/auth/google/auth/google/callback', passport.authenticate('google',{ session: false }), (req, res) => {
+app.get('/auth/google/callback', passport.authenticate('google',{ session: false }), (req, res) => {
   // Generate JWT token
   const accessToken = jwt.sign({ email: req.user.email, password: 'password' }, jwtSecret, { expiresIn: '7d' });
   const refreshToken = jwt.sign({ email: req.user.email, password: 'password' }, jwtSecret, { expiresIn: '7d' });
@@ -100,6 +81,8 @@ app.get('https://wild-red-clownfish-tam.cyclic.app/auth/google/auth/google/callb
   });
 
 });
+
+
 
 
 module.exports = app;
