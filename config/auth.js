26b2f828f6083e148c481ebabe8e21d9359cc62f
Sree Chakram <srebhargav.g@avyata.com>
2023-06-30 18:33:00 +0530

    bin file updated

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     googleAuth: {
       clientID: '106381978093-gptl8utmu520kd6dqesdavq0vpb9s3mu.apps.googleusercontent.com',
       clientSecret: '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',
-      callbackURL: 'http://localhost:8000/auth/google/callback' // Replace with your callback URL
+      callbackURL: 'http://localhost:8000/auth/google/callback' || 'https://wild-red-clownfish-tam.cyclic.app/auth/google/callback'// Replace with your callback URL
     },
     jwtSecret: 'YOUR_JWT_SECRET' // Replace with your preferred secret key for JWT
   };
